da01c112678542b7c6053fa2537af238ad6789d3
Davor Grubelic <davor@uxpassion.com>
2016-09-23 12:36:08 +0200

    added compose content component

--- client/components/compose-content/compose-content.js
+++ client/components/compose-content/compose-content.js
@@ -5,58 +5,91 @@ const config = require('./config.json');
  * IndexController
  */
 class ComposeContentController {
-  constructor($element) {
+  constructor($element, TweetService) {
     'ngInject';
     this._element = $element[0];
     this._$element = $element;
+    this.TweetService = TweetService;
 
     this.tweetForm = {
       content: null
     };
-    this.onContentKeyupBind = this.onContentKeyup.bind(this);
-    this.onContentFocusBind = this.onContentFocus.bind(this);
-    this.onContentBlurBind = this.onContentBlur.bind(this);
+
+    if (this.replyTo) {
+      this.tweetForm.content = '@' + this.replyTo.user.screen_name + '&nbsp;';
+    }
+
+    this.onElementClickBind = this.onElementClick.bind(this);
+    this.onElementKeydownBind = this.onElementKeydown.bind(this);
+    this.onContentClickBind = this.onContentClick.bind(this);
+    this.onToolbarClickBind = this.onToolbarClick.bind(this);
+    this.onDocumentClickBind = this.onDocumentClick.bind(this);
   }
 
   $onInit() {
-    let $contentInput = this._element
-      .querySelector(config.selectors.CONTENT_INPUT)
+    let $contentInput = this._$element.find(config.selectors.CONTENT_INPUT);
+    let $contentToolbar = this._$element.find(config.selectors.CONTENT_TOOLBAR);
 
-    $contentInput.addEventListener('keyup', this.onContentKeyupBind)
-    $contentInput.addEventListener('focus', this.onContentFocusBind)
-    $contentInput.addEventListener('blur', this.onContentBlurBind)
+    this._$element.on('click', this.onElementClickBind);
+    this._$element.on('keydown', this.onElementKeydownBind);
+    $contentInput.on('click', this.onContentClickBind)
+    $contentToolbar.on('click', this.onToolbarClickBind);
   }
 
   $onDestroy() {
-    let $contentInput = this._element
-      .querySelector(config.selectors.CONTENT_INPUT)
+    let $contentInput = this._$element
+      .find(config.selectors.CONTENT_INPUT)
+    let $contentToolbar = this._$element.find(config.selectors.CONTENT_TOOLBAR);
 
-    $contentInput.removeEventListener('keyup', this.onContentKeyupBind)
-    $contentInput.removeEventListener('focus', this.onContentFocusBind)
-    $contentInput.removeEventListener('blur', this.onContentBlurBind)
+    this._$element.off('click', this.onElementClickBind);
+    this._$element.off('keydown', this.onElementKeydownBind);
+    $contentInput.off('click', this.onContentClickBind)
+    $contentToolbar.off('click', this.onToolbarClickBind);
   }
 
-  onContentFocus() {
+  onElementClick(e) {
+    if (this._$element.hasClass(config.cssClasses.IS_FOCUSED)) {
+      e.stopPropagation();
+    }
+  }
+
+  onElementKeydown(e) {
+    if (e.keyCode === 27 && this._$element.hasClass(config.cssClasses.IS_FOCUSED)) {
+      e.stopPropagation();
+    }
+  }
+
+  onContentClick(e) {
+    e.stopPropagation();
     this._$element.addClass(config.cssClasses.IS_FOCUSED);
+
+    setTimeout(() => {
+      document.addEventListener('click', this.onDocumentClickBind);
+    });
   }
 
-  onContentBlur() {
+  onToolbarClick(e) {
+    e.stopPropagation();
+  }
+
+  onDocumentClick(e) {
     this._$element.removeClass(config.cssClasses.IS_FOCUSED);
     if (this.tweetForm.content) {
       this._$element.addClass(config.cssClasses.IS_DIRTY);
     } else {
       this._$element.removeClass(config.cssClasses.IS_DIRTY);
     }
-  }
-
-  onContentKeyup() {
-
+    document.removeEventListener('click', this.onDocumentClickBind);
   }
 
   submitContent() {
     this.tweetForm.content = null;
     this._$element.removeClass(config.cssClasses.IS_FOCUSED);
     this._$element.removeClass(config.cssClasses.IS_DIRTY);
+
+    let $createdTweet = { text: this.tweetForm.content };
+    this.TweetService.createTweet($createdTweet);
+    this.onCreate($createdTweet);
   }
 
 };
@@ -64,5 +97,9 @@ class ComposeContentController {
 
 module.exports = {
   templateUrl: 'views/compose-content/compose-content.html',
-  controller: ComposeContentController
+  controller: ComposeContentController,
+  bindings: {
+    replyTo: '=',
+    onCreate: '&'
+  }
 };
