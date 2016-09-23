1d40c0ec574344f740b273ea5989e95bb0955fa5
Davor Grubelic <davor@uxpassion.com>
2016-09-23 12:38:25 +0200

    composed application

--- client/app.js
+++ client/app.js
@@ -6,6 +6,7 @@ const indexComponent = require('./components/index');
 // Initialize application
 const app = angular.module('ask-youth', [
   // resources
+  'ngSanitize',
   'ui.router',
   'permission', 'permission.ui'
 ]);
@@ -18,6 +19,7 @@ app.config(require('./config.js'))
 app.component('application', require('./components/application/application.js'))
 app.component('indexComponent', require('./components/index/index.js'))
 app.component('handlesComponent', require('./components/handles/handles.js'))
+app.component('handleComponent', require('./components/handle/handle.js'))
 app.component('topicsComponent', require('./components/topics/topics.js'))
 app.component('conversationsComponent', require('./components/conversations/conversations.js'))
 app.component('infographicsComponent', require('./components/infographics/infographics.js'))
@@ -27,11 +29,16 @@ app.component('loginComponent', require('./components/login/login.js'))
 // Inline components
 app.component('composeContentModal', require('./components/compose-content-modal/compose-content.js'));
 app.component('composeContent', require('./components/compose-content/compose-content.js'));
-app.component('tweetContent', require('./components/content/tweet.js'));
+app.component('timeline', require('./components/timeline/timeline.js'));
+app.component('tweet', require('./components/tweet/tweet.js'));
+app.component('tweetModal', require('./components/tweet-modal/tweet-modal.js'));
 
 // Directives
 app.directive('contenteditable', require('./components/shared/contenteditable.js'));
 app.directive('contentInput', require('./components/shared/content-input.js'));
+
+// Services
+app.service('TweetService', require('./components/tweet/tweet-service.js'));
 
 // Run application
 app.run(require('./roles.js'));
--- client/router.js
+++ client/router.js
@@ -8,31 +8,69 @@ module.exports = function ($stateProvider) {
   $stateProvider.state('index', {
     url: '/',
     template: '<index-component></index-component>',
-    data: { permissions: { only: ['user'], redirectTo: 'login' } }
+    data: { permissions: { only: ['user'], redirectTo: 'login' } },
+    onEnter: function ($document) {
+      'ngInject';
+      $document[0].title = 'Home • Ask Youth';
+    }
   })
 
   $stateProvider.state('handles', {
     url: '/handles',
     template: '<handles-component></handles-component>',
-    data: { permissions: { only: ['user'], redirectTo: 'login' } }
+    data: { permissions: { only: ['user'], redirectTo: 'login' } },
+    onEnter: function ($document) {
+      'ngInject';
+      $document[0].title = 'Handles • Ask Youth';
+    }
+  })
+
+  $stateProvider.state('handle', {
+    url: '/handle/:id',
+    template: '<handle-component></handle-component>',
+    data: { permissions: { only: ['user'], redirectTo: 'login' } },
+    resolve: {
+      handleModel: function ($stateParams) {
+        'ngInject';
+        return {
+          id: $stateParams.id
+        };
+      }
+    },
+    onEnter: function ($document, handleModel) {
+      'ngInject';
+      $document[0].title = handleModel.id + ' • Ask Youth';
+    }
   })
 
   $stateProvider.state('topics', {
     url: '/topics',
     template: '<topics-component></topics-component>',
-    data: { permissions: { only: ['user'], redirectTo: 'login' } }
+    data: { permissions: { only: ['user'], redirectTo: 'login' } },
+    onEnter: function ($document) {
+      'ngInject';
+      $document[0].title = 'Topics • Ask Youth';
+    }
   })
 
   $stateProvider.state('conversations', {
     url: '/conversations',
     template: '<conversations-component></conversations-component>',
-    data: { permissions: { only: ['user'], redirectTo: 'login' } }
+    data: { permissions: { only: ['user'], redirectTo: 'login' } },
+    onEnter: function ($document) {
+      'ngInject';
+      $document[0].title = 'Conversations • Ask Youth';
+    }
   })
 
   $stateProvider.state('infographics', {
     url: '/infographics',
     template: '<infographics-component></infographics-component>',
-    data: { permissions: { only: ['user'], redirectTo: 'login' } }
+    data: { permissions: { only: ['user'], redirectTo: 'login' } },
+    onEnter: function ($document) {
+      'ngInject';
+      $document[0].title = 'Infographics • Ask Youth';
+    }
   })
 
   /**
@@ -42,13 +80,21 @@ module.exports = function ($stateProvider) {
    $stateProvider.state('profile', {
      url: '/profile',
      template: '<profile-component></profile-component>',
-     data: { permissions: { only: ['user'] } }
+     data: { permissions: { only: ['user'] } },
+     onEnter: function ($document) {
+       'ngInject';
+       $document[0].title = 'Profile • Ask Youth';
+     }
    })
 
    $stateProvider.state('login', {
      url: '/login',
      template: '<login-component></login-component>',
-     data: { permissions: { only: ['guest'], redirectTo: 'index' } }
+     data: { permissions: { only: ['guest'], redirectTo: 'index' } },
+     onEnter: function ($document) {
+       'ngInject';
+       $document[0].title = 'Login • Ask Youth';
+     }
    })
 
    $stateProvider.state('logout', {
