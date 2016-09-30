cf3e07db33474cbd428c322ad8934a96fe52fccb
Davor Grubelic <davor@uxpassion.com>
2016-09-30 10:16:38 +0200

    Api call config

--- client/config.js
+++ client/config.js
@@ -1,11 +1,14 @@
 'use strict';
 const config = require('../config');
 
-module.exports = function ($urlRouterProvider, $locationProvider, $compileProvider, toastrConfig) {
+module.exports = function ($urlRouterProvider, $locationProvider, $compileProvider, $httpProvider, toastrConfig) {
   'ngInject';
   $urlRouterProvider.otherwise('/');
   $locationProvider.html5Mode(true);
   $compileProvider.debugInfoEnabled(config.get('app.debug'));
+
+  $httpProvider.defaults.withCredentials = false;
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
 
   angular.extend(toastrConfig, {
 
