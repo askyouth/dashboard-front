c4cca2539460a49aba6b2280f38c77710f61b2e9
David Jelic <david@uxpassion.com>
2017-05-31 10:36:03 +0200

    add link on ask logo to load broker timeline

--- client/components/index/components/global-stats-module.js
+++ client/components/index/components/global-stats-module.js
@@ -4,15 +4,20 @@
  * IndexController
  */
 class GlobalStatsModuleController {
-  constructor($element, AnalyticsService) {
+  constructor($element, AnalyticsService, ApiService) {
     'ngInject';
     this._$element = $element;
     this._AnalyticsService = AnalyticsService;
+    this._ApiService = ApiService
   }
 
   $onInit() {
     this._AnalyticsService.globalStats().then((globalStats) => {
       this.globalStats = globalStats;
+    })
+    this._ApiService.get('/account').then((response) => {
+      this.account = response.data;
+      this.account.handle = this.account.handle[0]
     })
   }
 };
@@ -21,7 +26,9 @@ class GlobalStatsModuleController {
 module.exports = {
   template: `
     <div class="module global-stats">
-      <div class="logo global-stats__logo"></div>
+      <div class="logo global-stats__logo">
+        <a class="global-stats__logo-link" ui-sref="handle({ id: $ctrl.account.handle.id })"></a>
+      </div>
 
       <div class="row global-stats__list">
         <a ui-sref="handles({ group: 2})" class="col-sm-4 global-stats__item">
